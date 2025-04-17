79fd085ecd6688a07424eb933641932ba8d7479d
arunesh srivastava <66473010+aviSrivastava01@users.noreply.github.com>
2025-04-17 13:55:55 +0530

    First Commit

---
+++ kafka-producer/readme.md
@@ -0,0 +1,11 @@
+
+Start Zookeeper Server
+>> Go to the path where kafka is isntalled
+
+sh bin/zookeeper-server-start.sh config/zookeeper.properties
+
+Start Kafka Server / Broker
+
+sh bin/kafka-server-start.sh config/server.properties
+
+# Kafka-producer-springboot
---
+++ kafka-producer/src/main/java/com/example/kafka_producer/service/KafkaMessagePublisher.java
@@ -0,0 +1,31 @@
+package com.example.kafka_producer.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class KafkaMessagePublisher {
+
+    @Autowired
+    private KafkaTemplate<String, Object> template;
+
+    public void sendMessageToTopic(String message){
+        CompletableFuture<SendResult<String, Object>> future = template.send("Avi", message);
+        //this will not wait for result to come
+        future.whenComplete((result,ex)->{
+            if (ex == null) {
+                System.out.println("Sent message=[" + message +
+                        "] with offset=[" + result.getRecordMetadata().offset() + "]");
+            } else {
+                System.out.println("Unable to send message=[" +
+                        message + "] due to : " + ex.getMessage());
+            }
+        });
+
+    }
+
+}
